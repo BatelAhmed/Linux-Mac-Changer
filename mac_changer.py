91bbeb82aef69b3fa8988b57856e5bccb85d1186
Batel <126113198+BatelAhmed@users.noreply.github.com>
2024-07-23 20:54:58 -0700

    Update mac_changer.py
    Refactoring of program getting mac address now moved to new function get_current_mac.
    
    Improved regular expression, was missing parentheses  '()' at the beginning and end of capturing the MAC address which could lead to leaks. They are now installed in the code which makes sure the MAC address is read more reliably.
    
    Bunch of little tweaks like return statements to make sure errors are captured for the program to output the correct statement as well.
    
    Summary:
    Adjusted Logic Flow in main code, Error handling, and validation

--- mac_changer.py
+++ mac_changer.py
@@ -9,7 +9,7 @@ def get_arguments():
     parser.add_argument("-i", "--interface", dest="interface", required=True, help="Interface to change its MAC Address")
     parser.add_argument("-m", "--mac", dest="new_mac", required=True, help="New MAC Address")
     args = parser.parse_args()
-    return args
+    return args  # Return args so we can use it
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC Address for " + interface + " to " + new_mac)
@@ -17,14 +17,27 @@ def change_mac(interface, new_mac):
     subprocess.run(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.run(["ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface]).decode('utf-8')
+    mac_address_search_result = re.search(r"(\w\w:\w\w:\w\w:\w\w:\w\w:\w\w)", ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+        return None
+
+# Get the arguments first
 args = get_arguments()
-change_mac(args.interface, args.new_mac)
 
-ifconfig_result = subprocess.check_output(["ifconfig", args.interface]).decode('utf-8')
-print(ifconfig_result)
-
-mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-if mac_address_search_result:
-    print("New MAC Address: " + mac_address_search_result.group(0))
+current_mac = get_current_mac(args.interface)
+if current_mac:
+    print("Current MAC is: " + current_mac)
+    change_mac(args.interface, args.new_mac)
+    new_mac = get_current_mac(args.interface)
+    if new_mac == args.new_mac:
+        print("[+] MAC address was successfully changed to " + new_mac)
+    else:
+        print("[-] MAC address did not change.")
 else:
-    print("Could not read MAC address.")
+    print("[-] Failed to read the current MAC address, cannot proceed with changing it.")
